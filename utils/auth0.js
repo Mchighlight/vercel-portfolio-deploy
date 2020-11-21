bf043fb278feaa3fcd60753ef960420933b9b936
Henry Huang <Mchighlight@gmail.com>
2020-11-21 08:02:50 -0500

    add namespace

--- utils/auth0.js
+++ utils/auth0.js
@@ -20,7 +20,7 @@ const auth0 = initAuth0({
 export default auth0;
 
 export const isAuthorized = (user, role) => {
-  return (user && user[process.env.AUTH0_NAMESPACE + '/roles'].includes(role));
+  return (user && user['https://portoflio-henry-com' + '/roles'].includes(role));
 }
 
 export const authorizeUser = async (req, res) => {
